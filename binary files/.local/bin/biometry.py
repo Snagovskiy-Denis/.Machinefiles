42f85ffaed4453de4836814c9768a9d0a44580e6
Denis Snagovskiy <denis.snagovskiy@list.ru>
2024-02-20 14:33:52 +0300

    2024-02-20

--- binary files/.local/bin/biometry.py
+++ binary files/.local/bin/biometry.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 import sqlite3
+import json
 
 from pathlib import Path
 from typing import Annotated
 from itertools import chain
+from enum import Enum
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from typer import Argument, Typer, Option, confirm, Exit
 from rich import print
+from rich.table import Table
+from rich.console import Console
 
 
 connection: sqlite3.Connection = None
@@ -49,7 +53,28 @@ VaultDB = Annotated[
 ]
 
 
-FROM_SQL = """
+class Resample(str, Enum):
+    weekly = "W-Mon"
+    monthly = "M"
+    quarterly = "Q"
+    yearly = "Y"
+
+    def label(self, text: str) -> str:
+        return f"{self.name.capitalize()} {text}"
+
+
+class Metric(str, Enum):
+    weight = "weight"
+    waist = "waist"
+
+
+SELECT_PROGRESS = f"""
+    SELECT
+        date(bio.timestamp, 'auto') AS day,
+        bio.weight,
+        bio.waist,
+        food_totals.kcal AS kcal,
+        steps.value AS steps
     FROM
         biometry AS bio
     LEFT JOIN
@@ -70,7 +95,7 @@ FROM_SQL = """
 
 
 @app.command()
-def weight(
+def plot(
     delta_days_from_now: Annotated[
         int,
         Argument(min=0, help="Enter 0 to show all days"),
@@ -78,61 +103,54 @@ def weight(
     exclude_today: bool = True,
     weight: bool = False,
     annotations: bool = True,
+    balance: bool = True,
     calories: bool = True,
     steps: bool = True,
+    resample: Resample = Resample.weekly,
     *,
     db: VaultDB,
 ):
-    "Shows weight dynamics"
+    "Plots a graph showing trends in weight and energy balance."
 
-    select_query = f"""
-    SELECT
-        date(bio.timestamp, 'auto') AS day,
-        bio.weight,
-        bio.waist,
-        food_totals.kcal AS kcal,
-        steps.value AS steps
-    {FROM_SQL}
-    """
+    select_query = SELECT_PROGRESS
     if delta_days_from_now:
         select_query += f"""
         WHERE
             bio.timestamp > unixepoch('now', '-{delta_days_from_now} day')
         """
         if exclude_today:
-            select_query += " AND bio.timestamp < unixepoch(date('now'), 'utc')"
+            select_query += " AND bio.timestamp < unixepoch(date('now'))"
+    select_query += " ORDER BY bio.timestamp"
     df = pd.read_sql_query(select_query, db.connection)
     df["day"] = pd.to_datetime(df["day"])
 
-    df_weekly = df.resample("W-Mon", on="day").mean()
-    df_weekly["weight_diff"] = df_weekly["weight"].diff()
-    waist_data = df[df["waist"].notnull()]
-
-    # weight_mean = df["weight"].mean()
+    df_resampled = df.resample(resample.value, on="day").mean()
+    df_resampled["weight_diff"] = df_resampled["weight"].diff()
 
     plots = []
     fig, ax_w_diff = plt.subplots()
 
     color_reg = "tab:red"
     line_1 = ax_w_diff.plot(
-        df_weekly.index,
-        df_weekly["weight"],
+        df_resampled.index,
+        df_resampled["weight"],
         color=color_reg,
-        label="Weekly Weight Change (kg)",
+        label=resample.label("Weight Change (kg)"),
         linewidth=2,
         marker="x",
     )
     ax_w_diff.tick_params(axis="y", labelcolor=color_reg)
     ax_w_diff.set_ylabel("kg", color=color_reg)
     if annotations:
-        for index, row in df_weekly.iterrows():
+        for index, row in df_resampled.iterrows():
+            bodyweight = row["weight"]
             weight_diff = row["weight_diff"]
             if not pd.isnull(weight_diff):
                 ax_w_diff.annotate(
-                    f"{weight_diff:.2f}",
+                    f"{weight_diff:.2f} ({weight_diff / bodyweight:.2%})",
                     xy=(index, row["weight"]),
                     textcoords="offset points",
-                    xytext=(10, 10),
+                    xytext=(35, 10),
                     ha="center",
                 )
     plots.append(line_1)
@@ -142,16 +160,15 @@ def weight(
     ax_waist.set_ylabel("cm", color=color_skyblue)
     ax_waist.yaxis.set_label_coords(1.05, 0.25)
     line_2 = ax_waist.plot(
-        waist_data["day"],
-        waist_data["waist"],
+        df_resampled.index,
+        df_resampled["waist"],
         color=color_skyblue,
-        label="Waist Girth (cm)",
+        label=resample.label("Waist Girth (cm)"),
         alpha=0.7,
         linewidth=2,
         marker="o",
     )
     ax_waist.set_ylim(bottom=85, top=115)
-    # ax_waist.set_aspect(weight_mean / waist_data["waist"].mean())
     ax_waist.tick_params(axis="y", labelcolor=color_skyblue)
     plots.append(line_2)
 
@@ -169,39 +186,38 @@ def weight(
         ax_weight.tick_params(axis="y", labelcolor=color_orange)
         plots.append(line_3)
 
-    if calories:
+    if balance and calories:
         ax_kcal = ax_w_diff.twinx()
         color4 = "green"
         ax_kcal.set_ylabel("kcal", color=color4)
         ax_kcal.yaxis.set_label_coords(1.05, 0.85)
         line_4 = ax_kcal.plot(
-            df_weekly.index,
-            df_weekly["kcal"],
+            df_resampled.index,
+            df_resampled["kcal"],
             color=color4,
             alpha=0.3,
-            label="Weekly Calories Intake (kcal)",
+            label=resample.label("Calories Intake (kcal)"),
         )
         ax_kcal.tick_params(axis="y", labelcolor=color4)
-        ax_kcal.set_ylim(bottom=1000, top=3000)
-        # ax_kcal.set_aspect(weight_mean / df["kcal"].mean())
+        ax_kcal.set_ylim(bottom=1000, top=2500)
         plots.append(line_4)
 
-    if steps:
+    if balance and steps:
         ax_steps = ax_w_diff.twinx()
         color5 = "brown"
         ax_steps.set_ylabel("steps", color=color5)
         line_5 = ax_steps.plot(
-            df_weekly.index,
-            df_weekly["steps"],
+            df_resampled.index,
+            df_resampled["steps"],
             color=color5,
             alpha=0.3,
-            label="Weekly Number of Steps (steps)",
+            label=resample.label("Number of Steps (steps)"),
         )
         ax_steps.tick_params(axis="y", labelcolor=color5)
         ax_steps.set_ylim(bottom=500, top=12000)
-        # ax_steps.set_aspect(weight_mean / df["steps"].mean())
         plots.append(line_5)
 
+    plots.append(plots.pop(1))  # resort waist
     plots = list(chain.from_iterable(plots))
     ax_w_diff.legend(plots, [p.get_label() for p in plots], loc="upper left")
 
@@ -213,43 +229,54 @@ def weight(
 
 
 @app.command()
-def today(db: VaultDB):
+def today(db: VaultDB, as_json: bool = False, rotate: bool = False):
     "Show todays stats"
 
-    select_query = f"""
-    SELECT
-        bio.weight IS NOT NULL AS weight_entered,
-        bio.waist IS NOT NULL AS waist_entered,
-        food_totals.kcal IS NOT NULL AS food_entered,
-        steps.value IS NOT NULL AS steps_entered
-    {FROM_SQL}
-    WHERE
-        date(bio.timestamp, 'auto') = date('now')
-    """
-    db.execute(select_query)
-    db.row_factory = sqlite3.Row
-    if result := db.fetchone():
-        for k, v in dict(result).items():
-            print(f"'{k}': {bool(v)}")
-    else:
-        print("No today's data :(")
+    db.execute(f"{SELECT_PROGRESS} WHERE date(bio.timestamp, 'auto') = date('now')")
+    db.row_factory = sqlite3.Row  # pyright: ignore
+
+    match db.fetchone(), as_json:
+        case None, True:
+            print(json.dumps(None))
+        case None, False:
+            print("No today' data :(")
+        case _ as result, True:
+            print(json.dumps(dict(result)))
+        case _ as result, False:
+            if not rotate:
+                table = Table(*result.keys())
+                table.add_row(*[v if v is None else str(v) for v in result])
+            else:
+                table = Table("metric", "value")
+                items = iter(dict(result).items())
+                table.title = next(items)[1]
+                for key, value in items:
+                    table.add_row(key, value if value is None else str(value))
+            Console().print(table)
 
 
 @app.command()
-def add(todays_weight: float, db: VaultDB):
-    "Add todays weight"
+def add(
+    todays_metric: float,
+    *,
+    metric: Annotated[Metric, Option("-m", prompt=True)],
+    db: VaultDB,
+):
+    "Add todays metric"
 
     today = "unixepoch(date('now'))"
-    db.execute(f"SELECT weight FROM biometry WHERE timestamp >= {today}")
+    db.execute(f"SELECT {metric.name} FROM biometry WHERE timestamp >= {today}")
     if row := db.fetchone():
-        db_weight = row[0]
-        if db_weight == todays_weight:
-            print(f"{db_weight} is already set as todays weight.")
+        db_metric = row[0]
+        if db_metric == todays_metric:
+            print(f"{db_metric} is already set as todays {metric.name}.")
             raise Exit()
-        confirm(f"Overwrite {db_weight} with {todays_weight}?", abort=True)
+        confirm(f"Overwrite {metric.name} {db_metric} with {todays_metric}?", abort=True)
 
-    qeury = f"INSERT OR REPLACE INTO biometry (timestamp, weight) VALUES({today},?)"
-    db.execute(qeury, [todays_weight])
+    query_i = f"INSERT OR IGNORE INTO biometry (timestamp, {metric.name}) VALUES({today},?)"
+    db.execute(query_i, [todays_metric])
+    query_u = f"UPDATE biometry SET {metric.name} = ? WHERE timestamp = {today}"
+    db.execute(query_u, [todays_metric])
     print("Done ✅")
 
 
--- binary files/.local/bin/etl/cronometer.py
+++ binary files/.local/bin/etl/cronometer.py
@@ -5,7 +5,6 @@ import logging
 
 from typing import Iterable
 from pathlib import Path
-from datetime import datetime
 from collections import defaultdict
 from itertools import chain
 
@@ -23,43 +22,53 @@ def to_SQL_list(items: Iterable) -> str:
     return f"({comma_separated_items})"
 
 
+def to_SQL_list_of_strings(strings: Iterable) -> str:
+    return to_SQL_list(f"'{string}'" for string in strings)
+
+
 def main(vault_db: Path, outer_csv: Path) -> int:
     csv_days = defaultdict(list)
     with open(outer_csv) as csvfile:
         reader = csv.reader(csvfile)
         csv_header = next(reader)
         for row in reader:
-            day = int(datetime.fromisoformat(row[0]).timestamp())
-            row[0] = day
+            day = row[0]
             csv_days[day].append(row)
+    logging.debug(f"found {len(csv_days)} days from csv")
 
     connection = sqlite3.connect(vault_db, timeout=10)
     cursor = connection.cursor()
 
     select_query = f"""
         SELECT
-            timestamp AS day,
+            date(timestamp, 'auto') AS day,
             SUM(energy__kcal) AS kcal
         FROM
             food_tracks
         WHERE
-            timestamp IN {to_SQL_list(csv_days)}
+            day IN {to_SQL_list_of_strings(csv_days)}
         GROUP BY
-            timestamp
+            day
     """
     cursor.execute(select_query)
     kcal_sums_in_db = {day: sum_ for day, sum_ in cursor.fetchall()}
+    logging.debug(f"{kcal_sums_in_db=}")
 
     days_to_import = defaultdict(list)
     for day, food_tracks in csv_days.items():
         if not kcal_sums_in_db.get(day):
+            logging.debug(f"{day} kcal in db IS NULL")
             days_to_import[day] = food_tracks
             continue
 
         CALORIES_INDEX = 3
         kcal_sum_in_csv = sum(float(track[CALORIES_INDEX]) for track in food_tracks)
-        if kcal_sum_in_csv > kcal_sums_in_db[day]:  # overwrite existing food_tracks
+        logging.debug(f"{day} {kcal_sum_in_csv = :.2f}, {kcal_sums_in_db[day] = :.2f}")
+        if int(kcal_sum_in_csv) > int(kcal_sums_in_db[day]):  # overwrite existing food_tracks
+            logging.debug(f"include {day} food tracks")
             days_to_import[day] = food_tracks
+        else:
+            logging.debug(f"exclude {day} food tracks")
 
     if not days_to_import.values():
         return 0
@@ -71,12 +80,18 @@ def main(vault_db: Path, outer_csv: Path) -> int:
             header = header.replace(*old_and_new)
         headers.append(header.lower())
 
+    values_bind = ["unixepoch(date(?))"] + ["?"] * (len(headers) - 1)
     insert_query = f"""
         INSERT OR IGNORE INTO food_tracks {to_SQL_list(headers)}
-        VALUES {to_SQL_list(["?"] * len(headers))}
+        VALUES {to_SQL_list(values_bind)}
     """
 
-    delete_query = f"DELETE FROM food_tracks WHERE timestamp IN {to_SQL_list(days_to_import)}"
+    delete_query = f"""
+    DELETE FROM
+        food_tracks
+    WHERE
+        date(timestamp, 'auto') IN {to_SQL_list_of_strings(days_to_import)}
+    """
 
     food_tracks_to_import = list(chain.from_iterable(days_to_import.values()))
 
