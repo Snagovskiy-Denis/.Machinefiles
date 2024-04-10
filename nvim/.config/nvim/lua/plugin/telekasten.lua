25fdd16d215d134670d3ee2c1e58a43a9516ef80
Denis Snagovskiy <denis.snagovskiy@list.ru>
2024-04-10 20:56:00 +0300

    2024-04-10

--- nvim/.config/nvim/lua/plugin/telekasten.lua
+++ nvim/.config/nvim/lua/plugin/telekasten.lua
@@ -1,4 +1,5 @@
-local home = vim.fn.expand(os.getenv 'ZETTELKASTEN') .. '../'
+local home = vim.loop.fs_realpath(os.getenv 'ZETTELKASTEN' .. '../')
+-- fix for https://github.com/renerocksai/telekasten.nvim/issues/264
 
 require('telekasten').setup {
     home = home,
@@ -6,18 +7,18 @@ require('telekasten').setup {
     -- dailies = home .. '../Journal',
     -- weeklies = home .. '../Journal',
     -- templates = home .. '../Templates',
-    dailies = home .. 'Journal',
-    weeklies = home .. 'Journal',
-    templates = home .. 'Templates',
+    dailies = home .. '/Journal',
+    weeklies = home .. '/Journal',
+    templates = home .. '/Templates',
 
     weeklies_create_nonexisting = false,
 
     -- template_new_note = home .. '../Templates/Mine Моё.md',
     -- template_new_daily = home .. '../Templates/Daily.md',
-    template_new_note = home .. 'Templates/Mine Моё.md',
-    template_new_daily = home .. 'Templates/Daily.md',
+    template_new_note = home .. '/Templates/Mine Моё.md',
+    template_new_daily = home .. '/Templates/Daily.md',
 
-    subdirs_in_links = false,
+    -- subdirs_in_links = false,
 
     plug_into_calendar = false,
 }
